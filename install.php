cc4213b86704c5e2f9b5d4c8381d2f342af63a7c
John Rayes <live627@gmail.com>
2020-03-18 18:46:44 -0700

    require php 7.1

--- install.php
+++ install.php
@@ -378,8 +378,8 @@ function populateDB()
 // !!! Requires PHP/5.3.0+!
 function versionCheck()
 {
-	if (version_compare(PHP_VERSION, '5.3.0', '<'))
-		fatal_error('This mod needs PHP 5.3 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
+	if (PHP_VERSION_ID < 70100)
+		fatal_error('This mod needs PHP 7.1 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
 }
 
 ?>
